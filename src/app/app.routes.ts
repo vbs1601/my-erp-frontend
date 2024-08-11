8974e57b5c2b158a38fd493b167108c94cc0288d
Vijay Sagar <88724782+vbs1601@users.noreply.github.com>
2024-08-11 23:36:27 +0530

    first comit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inventory', component: InventoryListComponent },
+    { path: '', redirectTo: '/inventory', pathMatch: 'full' },
+]
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+ 
